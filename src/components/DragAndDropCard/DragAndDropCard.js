54c93733f0e7129995f22897927dd9c625473139
Jon Jackson <ocskier@gmail.com>
2022-05-14 17:20:10 -0500

    Builds out card content

--- src/components/DragAndDropCard/DragAndDropCard.js
+++ src/components/DragAndDropCard/DragAndDropCard.js
@@ -11,7 +11,24 @@ const DragAndDropCard = ({ id, status, title, handleDrag }) => {
         <p className="card-header-title">
           {status[0].toUpperCase() + status.slice(1)}
         </p>
+        <button className="card-header-icon" aria-label="more options">
+          <span className="icon">
+            <i className="fas fa-angle-down" aria-hidden="true"></i>
+          </span>
+        </button>
       </header>
+      <div className="card-content">
+        <div className="content">
+          {title}
+          <br />
+          <time dateTime="2016-1-1">Timestamp</time>
+        </div>
+      </div>
+      <footer className="card-footer">
+        <a href="#" className="card-footer-item">
+          View
+        </a>
+      </footer>
     </div>
   );
 };
