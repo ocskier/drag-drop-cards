1d6e3642e2822e5b11cf18d2e6ef0dc2c6e3b9fa
Jon Jackson <ocskier@gmail.com>
2022-05-15 09:09:20 -0500

    Adds test data

--- config-overrides.js
+++ config-overrides.js
@@ -4,6 +4,7 @@ module.exports = function override(config, env) {
   //do stuff with the webpack config...
   config.resolve.alias = {
     "@components": path.resolve(__dirname, "./src/components"),
+    "@data": path.resolve(__dirname, "./src/data"),
   };
   return config;
 };
